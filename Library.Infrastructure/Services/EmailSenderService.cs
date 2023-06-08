0f37353c30b94bd84f9a105e5c3de3bb6707b242
Felix Jose Ynfante Quezada <felixjoseynfantequezada@gmail.com>
2023-06-08 15:50:23 -0400

    otro cambio en master

--- Library.Infrastructure/Services/EmailSenderService.cs
+++ Library.Infrastructure/Services/EmailSenderService.cs
@@ -11,6 +11,7 @@ namespace Library.Infrastructure.Services
 {
     //adding from master
     //adding other thing
+    //master modificado
     public class EmailSenderService : IEmailSenderService
     {
         //cambio en develop
